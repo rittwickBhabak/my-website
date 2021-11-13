5288a6afa8c2c948506c43ecf7831d13a54c4d16
Rittwick Bhabak <rittwick2019@gmail.com>
2021-11-14 00:13:46 +0530

    admin headers added

--- choti_golpo/admin.py
+++ choti_golpo/admin.py
@@ -2,9 +2,9 @@ from django.contrib import admin
 from .models import Story, DirtyTalk
 
 
-# admin.site.site_header = 'Choti Golop Admin'
-# admin.site.site_title = 'Choti Golop Admin Area'
-# admin.site.index_title = 'Welcome to the Choti Golop admin area'
+admin.site.site_header = 'I am Rittwick'
+admin.site.site_title = 'I am Rittwick'
+admin.site.index_title = 'Welcome to I am Rittwick'
 
 admin.site.register(Story)
 admin.site.register(DirtyTalk)
